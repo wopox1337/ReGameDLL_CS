05cf0f797dfb602ca302584549f0003fe98d0f5f
Shorohov Sergey <wopox1337@ya.ru>
2020-02-19 19:38:52 +0300

    Fix fadetoblack issues (#507)

--- regamedll/dlls/observer.cpp
+++ regamedll/dlls/observer.cpp
@@ -6,7 +6,11 @@ int __API_HOOK(GetForceCamera)(CBasePlayer *pObserver)
 {
 	int retVal;
 
+#ifndef REGAMEDLL_ADD
 	if (!fadetoblack.value)
+#else
+	if (fadetoblack.value == FADETOBLACK_STAY)
+#endif
 	{
 		retVal = int(CVAR_GET_FLOAT("mp_forcechasecam"));
 
@@ -39,7 +43,11 @@ void UpdateClientEffects(CBasePlayer *pObserver, int oldMode)
 {
 	bool clearProgress = false;
 	bool clearBlindness = false;
+#ifndef REGAMEDLL_ADD
 	bool blindnessOk = (fadetoblack.value == 0);
+#else
+	bool blindnessOk = (fadetoblack.value != FADETOBLACK_STAY);
+#endif
 	bool clearNightvision = false;
 
 	if (pObserver->GetObserverMode() == OBS_IN_EYE)
